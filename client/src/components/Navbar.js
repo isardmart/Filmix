40a9178c1fd09cd2a9e417b6697408c71193e51a
isardmart <isardmart@gmail.com>
2022-12-12 19:36:25 +0100

    Step by step

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-        <NavLink exact to='/' className={(navData) => (navData.isActive ? "active" : 'navlink')}>Home</NavLink>
-        <NavLink exact to='/login' className={(navData) => (navData.isActive ? "active" : 'navlink')}>Login</NavLink>
-        <NavLink exact to='/register' className={(navData) => (navData.isActive ? "active" : 'navlink')}>Register</NavLink>
-        <NavLink exact to='/secret' className={(navData) => (navData.isActive ? "active" : 'navlink')}>Secret</NavLink>
+    <div className="navbar">
+      <NavLink
+        exact
+        to="/"
+        className={(navData) => (navData.isActive ? "active" : "navlink")}
+      >
+        Home
+      </NavLink>
+      <ul>
+        <NavLink
+          exact
+          to="/login"
+          className={(navData) => (navData.isActive ? "active" : "navlink")}
+        >
+          Login
+        </NavLink>
+        <NavLink
+          exact
+          to="/register"
+          className={(navData) => (navData.isActive ? "active" : "navlink")}
+        >
+          Register
+        </NavLink>
+      </ul>
     </div>
   );
 }
-
+//<NavLink exact to='/secret' className={(navData) => (navData.isActive ? "active" : 'navlink')}>Secret</NavLink>
