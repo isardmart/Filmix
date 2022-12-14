5acdbbb6a0f4a206764b12ab3276e13d8cea5d9d
isardmart <isardmart@gmail.com>
2022-12-14 12:06:07 +0100

    let's see vercel

--- client/src/views/Movies.js
+++ client/src/views/Movies.js
@@ -10,43 +10,59 @@ export default function Movies({logout}) {
   )
 }
 */
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import {apikey} from '../config';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const App =()=> {
-const [title, setTitle]=useState('')
-const [year, setYear]=useState('')
-const [error, setError]=useState('')
+const Movies = () => {
+  const [ error, setError ] = useState('');
+  const [ isReady, setIsReady ] = useState(false);
+  const [ loading, setLoading ] = useState(false);
+  const [ movie, setMovie ] = useState({
+    title: '',
+    year: '',
+    movie: ''
+  });
 
-const findMovie = movie => {
-  let url = `https://omdbapi.com?t=${movie}&apikey=thewdb`;
-  axios
-    .get(url)
-    .then(res => {
-      debugger
-      let { Title, Year } = res.data;
-      setTitle(Title)
-      setYear(Year)
-    })
-    .catch(error => {
-      debugger;
-      console.log(error)
+  const handleChange = (e) => setMovie({ ...movie, movie: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    findMovie(movie.movie);
+  };
+  const findMovie = async (movie) => {
+    let url = `https://omdbapi.com?t=${movie}&apikey=${apikey}`;
+    try {
+      const res = await axios.get(url);
+      let { Title, Year, Error } = res.data;
+      setMovie({ title: Title, year: Year, movie: '' });
+      setIsReady(true);
+      setError(Error);
+    } catch (error) {
       setError(error.message);
-    });
+      setIsReady(true);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={movie.movie} />
+        <button>Submit</button>
+      </form>
+      {isReady ? !error ? (
+        <div>
+          <h1>Title:{movie.title}</h1>
+          <h1>Release year:{movie.year}</h1>
+        </div>
+      ) : (
+        <h1>{error}</h1>
+      ) : loading ? (
+        <img src="https://www.organizedthemes.com/wp-content/plugins/remind-me/js/loading.gif" alt="loading" />
+      ) : null}
+    </div>
+  );
 };
 
-  useEffect(()=>{
-    findMovie("The usual suspects")
-  },[]
-  )
-  
-    return (
-      <div>
-         <h1>Title:{title}</h1>
-        <h1>Release year:{year}</h1> 
-         {error && <h1>Oops, we have an error: {error}</h1>} 
-      </div>
-    );
-  }
-
-export default App
+export default Movies;
