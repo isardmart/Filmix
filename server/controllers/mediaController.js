4eeefea5eb75312e68cafa7d3b916f6e5eec9259
isardmart <isardmart@gmail.com>
2022-12-16 18:31:33 +0100

    api keys

--- server/controllers/mediaController.js
+++ server/controllers/mediaController.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const search= async (req,res) => {
     const {title} =req.body;
-      let url = `https://omdbapi.com?t=${title}&apikey=${apikey}`;
+      let url = `https://omdbapi.com?t=${title}&apikey=${process.env.apikey}`;
       try {
         const fetch = await axios.get(url);
         return res.json({ ok:true, media:fetch.data});
--- server/controllers/mediaController2.js
+++ server/controllers/mediaController2.js
@@ -2,21 +2,21 @@ const axios = require('axios');
 
 const search= async (req,res) => {
     const {imdbid} =req.body;
-      let url = `https://api.themoviedb.org/3/find/${imdbid}?api_key=${apikey2}&language=en-US&external_source=imdb_id`;
+      let url = `https://api.themoviedb.org/3/find/${imdbid}?api_key=${process.env.apikey2}&language=en-US&external_source=imdb_id`;
       try {
         const fetch = await axios.get(url);
         return res.json({ ok:true, media:fetch.data});
       } catch (error) {
-        return res.json(error);
+        return res.error(error);
       }
     };
 const trending=async(req,res)=>{
-  let url =`https://api.themoviedb.org/3/trending/all/day?api_key=${apikey2}`
+  let url =`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.apikey2}`
   try {
     const fetch =await axios.get(url);
     return res.json({ ok:true, media:fetch.data});
   } catch (error) {
-    
+    return res.error(error)
   }
 }
   
